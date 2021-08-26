a19142e9b8350eb58f4ed78a367e24eafd654557
René Francisco Ramos Lazo <franklazo32@gmail.com>
2021-08-26 10:27:47 -0600

    Agregar archivos de proyecto.

---
+++ SistemaLicenci_SuppCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaLicenci_SuppCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
